efbd204b696c50b3441172f9db64208f6d9a8a44
Alexey Rykhalskiy <alexey.rykhalskiy@gmail.com>
2018-10-24 19:44:08 +0300

    ServletAA

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,8 +1,16 @@
 import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.servlet.ServletContextHandler;
+import org.eclipse.jetty.servlet.ServletHolder;
 
 public class Application {
     public static void main(String[] args) throws Exception {
         Server server = new Server(8080);
+
+
+        //server.setHandler(new ServletContextHandler(){{
+        //    addServlet(new ServletHolder(new ServletAssets()),"/assets/*");
+
+
         server.start();
         server.join();
     }
