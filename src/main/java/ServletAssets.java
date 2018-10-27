b7b65299fc31a344382d14eb171828c5901440a8
Alexey Rykhalskiy <alexey.rykhalskiy@gmail.com>
2018-10-27 11:05:55 +0300

    lesson #2

--- src/main/java/ServletAssets.java
+++ src/main/java/ServletAssets.java
@@ -6,19 +6,23 @@ import javax.servlet.ServletOutputStream;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.File;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 public class ServletAssets extends HttpServlet {
     static Logger log = LoggerFactory.getLogger(ServletAssets.class);
-    private final static String PATH = "./static";
+    private final static String PATH = "static";
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         log.info("=============");
-        log.info(req.getServletPath());
-        log.info(req.getPathInfo());
+        File f = new File(PATH, req.getPathInfo());
+        log.info(f.getAbsolutePath());
         log.info("=============");
+        Files.copy(Paths.get(PATH, req.getPathInfo()), resp.getOutputStream());
         //PrintWriter writer = resp.getWriter();
         //ServletOutputStream outputStream = resp.getOutputStream();
     }
